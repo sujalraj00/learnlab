3553a9681bd5ac788f8039ba58f9a7660005d415
Sujal Raj <sujalraj611@gmail.com>
2025-02-07 23:14:54 +0530

    added file but need some refactor

---
+++ lib/auth/cubit/auth_state.dart
@@ -0,0 +1,16 @@
+part of 'auth_cubit.dart';
+
+sealed class AuthState {}
+
+final class AuthInitial extends AuthState {}
+
+final class AuthLoading extends AuthState {}
+
+final class AuthSignUp extends AuthState {}
+
+final class AuthLoggedIn extends AuthState {}
+
+final class AuthError extends AuthState {
+  final String message;
+  AuthError(this.message);
+}
